9785f0cac4217819858122e8f11a4880e0b3055e
yasuda-ikue <ikue_yasuda@dwango.co.jp>
2026-05-22 13:44:14 +0900

    svelte-config修正

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,11 +7,18 @@ const config = {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
+	preprocess: vitePreprocess(),
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// adapter-static only supports static site generation, see https://svelte.dev/docs/kit/adapter-static for more information.
+		// If your environment requires server-side rendering, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+      pages: 'out',
+      assets: 'out',
+      fallback: '404.html', // SPAモードを併用する場合に推奨
+      precompress: false,
+      strict: true,
+    })
 	}
 };
 
