33b22ff268fe97e73609542f28f7b0e87f3acbd3
yasuda-ikue <ikue_yasuda@dwango.co.jp>
2026-05-22 13:56:54 +0900

    修正

--- svelte.config.js
+++ svelte.config.js
@@ -3,23 +3,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
-	preprocess: vitePreprocess(),
-	kit: {
-		// adapter-static only supports static site generation, see https://svelte.dev/docs/kit/adapter-static for more information.
-		// If your environment requires server-side rendering, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
+  compilerOptions: {
+    // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+    runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true),
+  },
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
       pages: 'out',
       assets: 'out',
-      fallback: '404.html', // SPAモードを併用する場合に推奨
+      fallback: '404.html',
       precompress: false,
       strict: true,
-    })
-	}
+    }),
+  },
 };
 
 export default config;
